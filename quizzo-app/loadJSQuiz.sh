d15d9bbc476ef98039aa09479718985942092ec2
Ken Rimple <krimple@chariotsolutions.com>
2013-03-22 12:40:33 -0400

    added fixes to build process

--- quizzo-app/loadJSQuiz.sh
+++ quizzo-app/loadJSQuiz.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pushd .
 cd quizzo-data-access
-mvn compile exec:java -Dexec.mainClass=org.springframework.data.examples.quizzo.util.QuizLoader -Dexec.args="../JavascriptQuiz.txt"
+mvn compile exec:java -Dexec.mainClass=org.phillyete.quizzo.util.QuizLoader -Dexec.args="../JavascriptQuiz.txt"
 popd 
